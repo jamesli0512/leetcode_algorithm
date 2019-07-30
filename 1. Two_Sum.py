fa7a906ffdbe33795a637cebbedc6abf59a258fd
Haotong(James) Li <47588270+jamesli0512@users.noreply.github.com>
2019-07-30 00:45:35 -0700

    Update 1. Two_Sum.py

--- 1. Two_Sum.py
+++ 1. Two_Sum.py
@@ -10,3 +10,12 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 '''
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash = {}
+        for i, num in enumerate(nums):
+            n = target - num
+            if n not in hash:
+                hash[num] = i
+            else:
+                return([hash[n], i])
